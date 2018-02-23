a65b233ab8d56197ef22cf8c417239b48d81cce7
Sanchez Romain <Romain@MacBook-Pro-de-Sanchez.local>
2018-02-23 15:32:08 +0100

    already corriged

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rosanche <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/09 15:57:06 by rosanche          #+#    #+#             */
+/*   Updated: 2017/11/09 16:28:29 by rosanche         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	else if (nb == 1 || nb == 0)
+		return (1);
+	while (nb > 1)
+		return (nb * ft_recursive_factorial(nb - 1));
+	return (0);
+}
